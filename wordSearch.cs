ff7a8b924b6f91f41e4cfaeb452cb6d276b579f7
FrancisH64 <hayesf3@gmail.com>
2018-11-27 19:04:32 -0500

    Update 1.0.1

--- wordSearch.cs
+++ wordSearch.cs
@@ -3,13 +3,12 @@ public int WordSearch(string content, string keyword)
 	int amount = 0; //Sets the amount to zero.
 	Parallel.For(0, content.Length, index =>
 	{
-		try //The try/catch block prevents the loop from going out of range.
+		if(content.Substring(index).Length >= keyword.Length) //Checks if the string is longer than the keyword, it keeps it bound.
 		{
 			string word = content.Substring(index, keyword.Length); //Takes in a word with the length of the keyword.
 			if (word.Equals(keyword)) //Checks if word is a match of keyword
 				amount++; //Increases the amount by 1.
 		}
-		catch {} //Nothing
 	});
 	return amount; //Returns the amount of keyword instances within content.
 }
